04a5e4d7c7cc5f5ef388e0980b282e52b3ae3a58
TechnoSwiss <technoswiss@typhoondigital.com>
2018-03-03 18:32:53 -0800

    Fixed up script so it would be compatible with Python 3, and Windows. Added comments to the Readme.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # MPMD-AutoBedLevel-Cal
-This python script will automatically adjust the endstop offset and delta radius for a Monoprice Mini Delta.
+This script builds on Dennis Browns Monoprice Mini Delta end-stop and delta radius calibration spreadsheet.
+
+It builds on bborncr's gcodesender.py script https://github.com/bborncr/gcodesender.py/blob/master/gcodesender.py
+and includes foosel's connection fix for OctoPrint https://github.com/OctoPrint/OctoPrint-MalyanConnectionFix/blob/master/octoprint_malyan_connection_fix/__init__.py
+
+This script is provided as-is without ANY warranty, expressed or implied, including its sutability for the task for which it was designed.
+If your printer tips up, throws up, blows up, burns up, or gets up and walks out the door, it's NOT my fault.
+Use at your own risk.
+
+Requirements:
+    Python 2.7 or Python 3 (Tested with Python 2.7.9 and Python 3.6.4)
+	pip install pyserial
+
+OS:
+    Linux (tested with Debian Jessie)
+	Windows (tested with Win10)
+	Mac (have not tested, but should work)
+
+
+I'm currently running this from my OctoPrint machine (an eeePC 701 running Debian, not a RaspPi),
+ if you're running this from your OctoPrint machine you'll need to run from an actual terminal (not the terminal tab on the web page).
+Make sure no other process has the serial port open (if on your OctoPrint machine make sure to disconnect OctoPrint from the printer)
+
+I wrote this script on too little sleep to save myself some time, it works, but it's not pretty and could be cleaned up quite a bit.
+If I waited till I felt it was ready though, I'd probably never release it, so here it is, warts and all.
+
+There is little error checking currently in the script (or rather none), so keep an eye on it while it's running.
+The script starts by setting the defaults from Dennis Brown's calibration walkthough (these can be changed in the script):
+    M92 X57.14 Y57.14 Z57.14
+	M666 X0.0 Y0.0 Z0.0
+	M665 R63.2
+
+It will then walk through up to 15 cycles of bed leveling, reading out the values and calculating new M666 and M665 R values until
+the error for all is less then 0.02. If it hasn't settled by the halfway point it will reduce the correction factor from just the
+error value, to half the error value. On ave. it's settled on a value for me in 7 runs, if it gets all the way to 15 it just ends
+leaving values were they were for the last run.
+
+After finishing it will print out the final settings on the printer. It will not store these values. You can issue an M500 command
+to save the to the EEPROM, or record them to use in a startup script. The delta radius value isn't restored on startup, so you'll
+need to record at least that value and re-enter it each time the printer is powered up.
--- auto_cal.py
+++ auto_cal.py
@@ -21,19 +21,20 @@ parser.add_argument('-r','--r-value',type=float,default=r_value,help='Starting r
 parser.add_argument('-s','--step-mm',type=float,default=step_mm,help='Set steps-/mm')
 args = parser.parse_args()
 
-if sys.platform != "win32": 
-    temp = serial.Serial(args.port, 115200, timeout=10, writeTimeout=10000, parity=serial.PARITY_ODD)
-port = serial.Serial(args.port, 115200, timeout=10, writeTimeout=10000,
-parity=serial.PARITY_NONE)
-if sys.platform != "win32":
+#if sys.platform != 'win32': 
+temp = serial.Serial(args.port, 115200, timeout=10, writeTimeout=10000, parity=serial.PARITY_ODD)
+if sys.platform == 'win32':
+    temp.close()
+port = serial.Serial(args.port, 115200, timeout=10, writeTimeout=10000, parity=serial.PARITY_NONE)
+if sys.platform != 'win32':
     temp.close()
 
 r_value = args.r_value
 step_mm = args.step_mm
 
-port.write("M92 X" + str(step_mm) + " Y" + str(step_mm) + " Z" + str(step_mm) + "\n")
-port.write("M666 X0.0 Y0.0 Z0.0\n")
-port.write("M665 R" + str(r_value) + "\n")
+port.write(('M92 X' + str(step_mm) + ' Y' + str(step_mm) + ' Z' + str(step_mm) + '\n').encode())
+port.write(('M666 X0.0 Y0.0 Z0.0\n').encode())
+port.write(('M665 R' + str(r_value) + '\n').encode())
 
 while True:
     settled = 0
@@ -41,119 +42,119 @@ while True:
 
     if runs > max_runs:
         break
-    print "Calibration run : " + str(runs) + "\n"
+    print('Calibration run : ' + str(runs) + '\n')
 
-    port.write("G28\n")
-    port.write("G29 P2 V4\n")
+    port.write(('G28\n').encode())
+    port.write(('G29 P2 V4\n').encode())
 
     while True:
-        out = port.readline()
-        if "G29 Auto Bed Leveling" in out:
+        out = port.readline().decode()
+        if 'G29 Auto Bed Leveling' in out:
             break
 
-    print "Z-Axis :"
-    out = port.readline()
-    z_axis_1 = out.split(" ")
-    print z_axis_1[6].rstrip()
-    out = port.readline()
-    z_axis_2 = out.split(" ")
-    print z_axis_2[6].rstrip()
-    z_ave = (float(z_axis_1[6]) + float(z_axis_2[6])) / 2
-    print str(z_ave) + '\n'
+    print('Z-Axis :')
+    out = port.readline().decode()
+    z_axis_1 = out.split(' ')
+    print(z_axis_1[6].rstrip())
+    out = port.readline().decode()
+    z_axis_2 = out.split(' ')
+    print(z_axis_2[6].rstrip())
+    z_ave = float("{0:.3f}".format((float(z_axis_1[6]) + float(z_axis_2[6])) / 2))
+    print(str(z_ave) + '\n')
     if check_max:
         max = z_ave
         axis = 0
 
-    print "X-Axis :"
-    out = port.readline()
-    x_axis_1 = out.split(" ")
-    print x_axis_1[6].rstrip()
-    out = port.readline()
-    x_axis_2 = out.split(" ")
-    print x_axis_2[6].rstrip()
-    x_ave = (float(x_axis_1[6]) + float(x_axis_2[6])) / 2
-    print str(x_ave) + '\n'
+    print('X-Axis :')
+    out = port.readline().decode()
+    x_axis_1 = out.split(' ')
+    print(x_axis_1[6].rstrip())
+    out = port.readline().decode()
+    x_axis_2 = out.split(' ')
+    print(x_axis_2[6].rstrip())
+    x_ave = float("{0:.3f}".format((float(x_axis_1[6]) + float(x_axis_2[6])) / 2))
+    print(str(x_ave) + '\n')
     if check_max:
         if x_ave > max:
             max = x_ave
             axis = 1
 
-    print "Y-Axis :"
-    out = port.readline()
-    y_axis_1 = out.split(" ")
-    print y_axis_1[6].rstrip()
-    out = port.readline()
-    y_axis_2 = out.split(" ")
-    print y_axis_2[6].rstrip()
-    y_ave = (float(y_axis_1[6]) + float(y_axis_2[6])) / 2
-    print str(y_ave) + '\n'
+    print('Y-Axis :')
+    out = port.readline().decode()
+    y_axis_1 = out.split(' ')
+    print(y_axis_1[6].rstrip())
+    out = port.readline().decode()
+    y_axis_2 = out.split(' ')
+    print(y_axis_2[6].rstrip())
+    y_ave = float("{0:.3f}".format((float(y_axis_1[6]) + float(y_axis_2[6])) / 2))
+    print(str(y_ave) + '\n')
     if check_max:
         if y_ave > max:
             max = y_ave
             axis = 2
         check_max = False
 
-    print "Center :"
-    out = port.readline()
-    center_1 = out.split(" ")
-    print center_1[6].rstrip()
-    out = port.readline()
-    center_2 = out.split(" ")
-    print center_2[6].rstrip()
-    c_ave = (float(center_1[6]) + float(center_2[6])) / 2
-    print str(c_ave) + '\n'
+    print('Center :')
+    out = port.readline().decode()
+    center_1 = out.split(' ')
+    print(center_1[6].rstrip())
+    out = port.readline().decode()
+    center_2 = out.split(' ')
+    print(center_2[6].rstrip())
+    c_ave = float("{0:.3f}".format((float(center_1[6]) + float(center_2[6])) / 2))
+    print(str(c_ave) + '\n')
 
     #target = [max, max, max]
     target = [z_ave, x_ave, y_ave]
 
-    z_error = z_ave - target[axis]
-    x_error = x_ave - target[axis]
-    y_error = y_ave - target[axis]
-    c_error = c_ave - ((z_ave + x_ave + y_ave) / 3)
-    print "Z-Error: " + str(z_error) + " X-Error: " + str(x_error) + " Y-Error: " + str(y_error) + " C-Error: " + str(c_error) + "\n"
+    z_error = float("{0:.4f}".format(z_ave - target[axis]))
+    x_error = float("{0:.4f}".format(x_ave - target[axis]))
+    y_error = float("{0:.4f}".format(y_ave - target[axis]))
+    c_error = float("{0:.4f}".format(c_ave - ((z_ave + x_ave + y_ave) / 3)))
+    print('Z-Error: ' + str(z_error) + ' X-Error: ' + str(x_error) + ' Y-Error: ' + str(y_error) + ' C-Error: ' + str(c_error) + '\n')
 
     if abs(z_error) >= 0.02 and axis != 0:
         trial_z = z_error + trial_z if runs < (max_runs / 2) else (z_error / 2) + trial_z
-        print "M666 Z" + str(trial_z) + "\n"
-        port.write("M666 Z" + str(trial_z) + "\n")
-        out = port.readline()
+        print('M666 Z' + str(trial_z) + '\n')
+        port.write(('M666 Z' + str(trial_z) + '\n').encode())
+        out = port.readline().decode()
     else:
         settled += 1
 
     if abs(x_error) >= 0.02 and axis != 1:
         trial_x = x_error + trial_x if runs < (max_runs / 2) else (x_error / 2) + trial_x
-        print "M666 X" + str(trial_x) + "\n"
-        port.write("M666 X" + str(trial_x) + "\n")
-        out = port.readline()
+        print('M666 X' + str(trial_x) + '\n')
+        port.write(('M666 X' + str(trial_x) + '\n').encode())
+        out = port.readline().decode()
     else:
         settled += 1
 
     if abs(y_error) >= 0.02 and axis != 2:
         trial_y = y_error + trial_y if runs < (max_runs / 2) else (y_error / 2) + trial_y
-        print "M666 Y" + str(trial_y) + "\n"
-        port.write("M666 Y" + str(trial_y) + "\n")
-        out = port.readline()
+        print('M666 Y' + str(trial_y) + '\n')
+        port.write(('M666 Y' + str(trial_y) + '\n').encode())
+        out = port.readline().decode()
     else:
         settled += 1
 
     if abs(c_error) >= 0.02:
-        r_value = r_value + c_error / -0.5
-        print "M665 R" + str(r_value) + "\n"
-        port.write("M665 R" + str(r_value) + "\n")
-        out = port.readline()
+        r_value = float("{0:.4f}".format(r_value + c_error / -0.5))
+        print('M665 R' + str(r_value) + '\n')
+        port.write(('M665 R' + str(r_value) + '\n').encode())
+        out = port.readline().decode()
     else:
         settled += 1
 
     if settled > 3:
         break
 
-port.write("M503\n")
+port.write(('M503\n').encode())
 
 while True:
-    out = port.readline()
-    if "M92" in out or "M666" in out or "M665" in out:
-        print out.strip()
-        if "M665" in out:
+    out = port.readline().decode()
+    if 'M92' in out or 'M666' in out or 'M665' in out:
+        print(out.strip())
+        if 'M665' in out:
             break
 
 port.close()
